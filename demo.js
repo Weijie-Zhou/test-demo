4df7cf89f7866221c3973eada308bdec55bd620b
Weijie-Zhou <13695859436@163.com>
2021-03-08 16:35:02 +0800

    修改dev分支的代码

--- demo.js
+++ demo.js
@@ -2,5 +2,6 @@ window.onload = function () {
     let box =document.getElementsByClassName('box')[0]
     box.addEventListener('click', () => {
         alert('这是测试代码')
+        console.log('这是dev分支')
     })
 }
